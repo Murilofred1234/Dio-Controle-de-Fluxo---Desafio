c075b4a8655434899c0021cdc4f138680ce60f4e
Murilo <murilofrederico18@gmail.com>
2024-05-10 12:45:55 -0300

    codigo com try catch e throw

---
+++ ControleFluxo/src/DesafioControleFluxo.java
@@ -0,0 +1,8 @@
+public class DesafioControleFluxo{
+	public static void main(String args[]){
+	
+
+
+		
+	}}
+	
---
+++ ControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends  Exception {
+
+    public ParametrosInvalidosException(){
+        System.out.println("OS PARAMETROS SÃO INVÁLIDOS");
+    };
+}
